0edc10dd6b63eac00b1351aa319461f8d8a069f3
Ashrut Panchal <ashrutpanchal111@gmail.com>
2024-09-27 00:57:36 +0530

    worked on cors

--- App.js
+++ App.js
@@ -16,7 +16,7 @@ app.use(cors());
 //     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
 //     credentials: true
 // }));
-app.options('*', cors()); 
+// app.options('*', cors()); 
 app.use(cookieParser())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
